1ae74dcaab0c3c1ddd637f6861c7f5b7e22fae4a
Misra-Praveen <vgipra@gmail.com>
2026-02-20 03:25:23 +0530

    fix bug

--- task-manager-backend/src/server.ts
+++ task-manager-backend/src/server.ts
@@ -1,28 +1,28 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import "dotenv/config";
 
-import authRoute from "./routes/authRoutes.js"
+import authRoute from "./routes/authRoutes.js";
 import taskRouter from "./routes/taskRoutes.js";
-
 
 const PORT = process.env.PORT || 5051;
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 app.use("/auth", authRoute);
-app.use("/tasks", taskRouter)
+app.use("/tasks", taskRouter);
 
-app.get("/",()=>{
-  console.log("API is running...")
-})
+app.get("/", (req, res) => {
+  console.log("API is running...");
+  res.send("Backend is working fine!");
+});
 app.use((err: any, req: any, res: any, next: any) => {
   console.error(err);
   res.status(500).json({ message: "Internal Server Error" });
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on PORT ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
